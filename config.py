025ef4cc786002cac2edb98f6c3621b4da8fdb62
denisVoll <denis_voll@mail.bg>
2023-03-20 11:59:37 +0000

    update config settings

--- config.py
+++ config.py
@@ -5,26 +5,25 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'secret-key'
 
-    BLOB_ACCOUNT = os.environ.get('BLOB_ACCOUNT') or 'cmsstoragepython'
-    BLOB_STORAGE_KEY = os.environ.get('BLOB_STORAGE_KEY') or 'E6dztiYDlow4Dut1tl+lsVsh6RyZWI0R9B1Yi8XnnhxZ4C5GqCosFAKPpH3Sbt8slOlDK+u18t0x+AStSAqw9g=='
-    BLOB_CONTAINER = os.environ.get('BLOB_CONTAINER') or 'images'
+    BLOB_ACCOUNT = os.environ.get('BLOB_ACCOUNT') or 'cmsblobudacity'
+    BLOB_STORAGE_KEY = os.environ.get('BLOB_STORAGE_KEY') or 'l97xWhJmKpi0XzhDWjknh14w+qAXDFECFx402/oxw+Aghfsz143gIeTOh5UDuuvUWkiNxsYQqea3+ASt3l+wzA=='
+    BLOB_CONTAINER = os.environ.get('BLOB_CONTAINER') or 'images1'
 
-    SQL_SERVER = os.environ.get('SQL_SERVER') or 'cmsprojectserver.database.windows.net'
-    SQL_DATABASE = os.environ.get('SQL_DATABASE') or 'CMSDB'
-    SQL_USER_NAME = os.environ.get('SQL_USER_NAME') or 'CMS'
-    SQL_PASSWORD = os.environ.get('SQL_PASSWORD') or 'Godfather2403'
+    SQL_SERVER = os.environ.get('SQL_SERVER') or 'udacitycmsserver2.database.windows.net'
+    SQL_DATABASE = os.environ.get('SQL_DATABASE') or 'CMS'
+    SQL_USER_NAME = os.environ.get('SQL_USER_NAME') or 'cmsAdmin'
+    SQL_PASSWORD = os.environ.get('SQL_PASSWORD') or 'Sako8338%'
 
     SQLALCHEMY_DATABASE_URI = 'mssql+pyodbc://' + SQL_USER_NAME + '@' + SQL_SERVER + ':' + SQL_PASSWORD + '@' + SQL_SERVER + ':1433/' + SQL_DATABASE  + '?driver=ODBC+Driver+17+for+SQL+Server'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    CLIENT_SECRET = "zJC8Q~O748U~yAj8NuUiN1v~giLqhFtHw4JglbF."
+    CLIENT_SECRET = "Ct28Q~wuQCMA3gCXvTq0Q7xp9iVZWja1pzoqoa~2"
  
     AUTHORITY = "https://login.microsoftonline.com/common"
 
-    CLIENT_ID = "b2540384-f0e5-4d02-a9be-010782318bfb"
+    CLIENT_ID = "1ba68d31-f9d5-49e2-857d-555fe631928a"
 
     REDIRECT_PATH = "/getAToken"
-
 
     SCOPE = ["User.Read"] 
 
